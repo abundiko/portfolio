34b761ea9253f83f97040172caeb8bfe594388fc
abundiko <abundiko111@gmail.com>
2023-03-10 14:09:38 +0100

    2:09pm 10/03/2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const PAGES = document.querySelectorAll('.page');
 const homePage = PAGES[0];
+const middleContent = document.getElementById('middle-content')
 
 window.onscroll = e => {
 
@@ -31,15 +32,52 @@ links.forEach((link) => {
   }
 });
 
-function checkUrl() {
+function checkUrl(animate) {
+  if (animate) {
+    DOM.remState('page-cover', 'd-none');
+    setTimeout(() => {
+      DOM.addState('page-cover-main', 'animate');
+      setTimeout(() => {
+        setTimeout(() => {
+
+          DOM.addState('page-cover', 'd-none');
+        }, 300);
+        DOM.remState('page-cover-main', 'animate');
+
+      }, 1000);
+    }, 100);
+  }
+
   const url = window.location.href;
+  middleContent.innerHTML = '';
   if (url.includes('#contact'))
     toPage('contact')
   else if (url.includes('#about'))
     toPage('about')
   else if (url.includes('#portfolio'))
     toPage('portfolio')
-  else toPage('home')
+  else {
+    toPage('home')
+    middleContent.innerHTML = `<div class="dots dflex dwrap djcsb dposa dparallax" data-parallax-speed="3"
+          style="width: 120px; left: 10vw; top: 2vh;">
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+          <dot class="d-inline-block rounded-circle bg1 mb-2" style="width: 30px; height: 30px;"></dot>
+        </div>
+        <div class="circle rounded-circle bg02 dposa dparallax"
+          style="height: 100px; width: 100px; right: 5vw; bottom: 15vh;" data-parallax-speed="1">
+        </div>
+        <div class="circle rounded-circle bg02 dposa dparallax" data-parallax-speed="0.5"
+          style="height: 150px; width: 150px; right: -70px; bottom: 5vh;">
+        </div>`;
+
+  }
 }
 
 function toPage(pageId) {
@@ -50,12 +88,15 @@ function toPage(pageId) {
   setTimeout(() => {
 
     DOM.addState(pageId, 'show');
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
   }, 300);
 }
 window.addEventListener('locationchange', function () {
   console.log('location changed!');
 });
 window.addEventListener('hashchange', () => {
-  checkUrl();
+  checkUrl(true);
 })
 checkUrl();
